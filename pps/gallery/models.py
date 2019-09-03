5a2e49e541cf55b7af837ff933e4fb08ceb76aef
TheAvidDev <AvidDev@protonmail.com>
2019-09-03 17:43:42 -0400

    Created Category model and index page

---
+++ pps/gallery/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey('Category', related_name='children', on_delete=models.CASCADE, blank=True, null=True)
