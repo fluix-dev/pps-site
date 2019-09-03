3aac609f8ac1b1ce2ec35bd0caa523b395193bec
TheAvidDev <AvidDev@protonmail.com>
2019-09-03 17:48:05 -0400

    Inline categories on admin page

--- pps/gallery/admin.py
+++ pps/gallery/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import *
 
+class CategoryInLine(admin.TabularInline):
+    model = Category
+    extra = 0
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    inlines = [
+        CategoryInLine,
+    ]
+
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
