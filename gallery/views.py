1929ff5c23ad48432ff50d72558f911b4746716b
TheAvidDev <AvidDev@protonmail.com>
2019-11-09 11:41:55 -0500

    Created gallery view

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,6 @@
+import os
+from os.path import isfile, join
+
 from django.shortcuts import render
 from .models import *
 
@@ -20,8 +23,14 @@ def category(request, category_id):
     return render(request, 'category.html', context)
 
 def gallery(request, gallery_id):
+    gallery = Gallery.objects.get(gallery_id=gallery_id)
+    root_url = os.path.join(settings.MEDIA_ROOT, str(gallery.category.category_id), str(gallery_id))
+    images = [f for f in os.listdir(root_url) if isfile(join(root_url, f))]
+
     context = {
         'parent_categories': Category.objects.filter(parent=None),
         'latest': Category.objects.all().exclude(parent=None)[0],
+        'base_url': settings.MEDIA_URL + str(gallery.category.category_id) + '/' + str(gallery_id) + '/',
+        'images': images
     }
-    return render(request, 'index.html', context)
+    return render(request, 'gallery.html', context)
