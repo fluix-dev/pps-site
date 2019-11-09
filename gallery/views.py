226f146e0ce59156bcdb3dafa12538043cd7e1cb
TheAvidDev <AvidDev@protonmail.com>
2019-11-09 07:50:19 -0500

    Added selected category name to category template

--- gallery/views.py
+++ gallery/views.py
@@ -10,10 +10,12 @@ def home(request):
     return render(request, 'index.html', context)
 
 def category(request, category_id):
+    category = Category.objects.get(category_id=category_id)
     context = {
         'parent_categories': Category.objects.filter(parent=None),
         'latest': Category.objects.all().exclude(parent=None)[0],
-        'galleries': Category.objects.get(category_id=category_id).galleries.all()
+        'category': category,
+        'galleries': category.galleries.all()
     }
     return render(request, 'category.html', context)
 
