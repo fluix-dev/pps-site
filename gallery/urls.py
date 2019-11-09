b7e9512d88f7302217094669affdbce8f009b8da
TheAvidDev <AvidDev@protonmail.com>
2019-11-09 07:44:11 -0500

    Added category view

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='index'),
-    path('c/<uuid:category_id>', views.category, name='category')
+    path('c/<uuid:category_id>', views.category, name='category'),
+    path('g/<uuid:gallery_id>', views.gallery, name='gallery')
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -12,6 +12,14 @@ def home(request):
 def category(request, category_id):
     context = {
         'parent_categories': Category.objects.filter(parent=None),
-        'latest': Category.objects.all().exclude(parent=None)[0]
+        'latest': Category.objects.all().exclude(parent=None)[0],
+        'galleries': Category.objects.get(category_id=category_id).galleries.all()
+    }
+    return render(request, 'category.html', context)
+
+def gallery(request, gallery_id):
+    context = {
+        'parent_categories': Category.objects.filter(parent=None),
+        'latest': Category.objects.all().exclude(parent=None)[0],
     }
     return render(request, 'index.html', context)
