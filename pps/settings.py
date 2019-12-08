a692e902268595aff390dfdb4377ce163d992311
TheAvidDev <AvidDev@protonmail.com>
2019-12-08 13:48:15 -0500

    Changed logging type

--- pps/settings.py
+++ pps/settings.py
@@ -77,20 +77,30 @@ WSGI_APPLICATION = 'pps.wsgi.application'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
     'handlers': {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
             'filename': 'debug.log',
+            'formatter': 'verbose'
         },
     },
     'loggers': {
         'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
+            'handlers':['file'],
             'propagate': True,
+            'level':'DEBUG',
         },
-    },
+    }
 }
 
 # Database
