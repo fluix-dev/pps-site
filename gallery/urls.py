fe57858663938aecb7b91ede3a33c355791948ab
TheAvidDev <AvidDev@protonmail.com>
2020-03-16 12:13:56 -0400

    Separate urls with comments

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,23 +3,32 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    # Static pages
     path('', views.home, name='index'),
+    path('help/', views.download_help, name='help'),
+    path('maintenance/', views.maintenance, name='maintenance'),
+
+    # Pre-ordering
     path('preorder/', views.preorder, name='preorder'),
     path('preorder/package', views.package, name='package'),
     path('preorder/individual', views.individual, name='individual'),
     path('preorder/checkout', views.checkout, name='checkout'),
     path('preorder/charge', views.charge, name='charge'),
+
+    # Contact
     path('contact/', views.contact, name='contact'),
     path('contact_post/', views.contact_post, name='contact_post'),
-    path('help/', views.download_help, name='help'),
+    
+    # Dynamic pages
     path('c/<uuid:category_id>', views.category, name='category'),
     path('g/<uuid:gallery_id>', views.gallery, name='gallery'),
     path('v/<uuid:gallery_id>', views.videos, name='videos'),
-    path('maintenance/', views.maintenance, name='maintenance'),
+
+    # Media files
     path('media/<uuid:category_id>/<uuid:gallery_id>/<file>',
-         views.serve_gallery_image, name='serve_gallery_image'),
+        views.serve_gallery_image, name='serve_gallery_image'),
     path('media/<uuid:category_id>/<uuid:gallery_id>/thumbnails/<file>',
-         views.serve_gallery_thumbnail, name='serve_gallery_thumbnail'),
+        views.serve_gallery_thumbnail, name='serve_gallery_thumbnail'),
     path('media/gallery_thumbnails/<file>',
-         views.serve_thumbnail, name='serve_thumbnail')
+        views.serve_thumbnail, name='serve_thumbnail')
 ]
